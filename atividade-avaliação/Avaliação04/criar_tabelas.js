bf3a9be3a83f8c704d5088135ffb0a225526a63e
leoschmitzqm <a947687206@sina.com>
2021-02-26 10:48:58 +0800

    teste01

---
+++ atividade-avaliação/Avaliação04/criar_tabelas.js
@@ -0,0 +1,29 @@
+const sqlite3 = require('sqlite3');
+
+const db = new sqlite3.Database('./Avaliação04/DB_PESSOAL_RH.db', (error) => {
+    if (error) console.log(error);
+});
+
+db.serialize(() => {
+    db.run('create table if not exists TB_PESSOAS (id integer primary key autoincrement , name text)', (error) => {
+        if (error) console.log(error);
+    });
+
+    db.run('insert into TB_PESSOAS (name) values (?)', ['João Alves Damasceno'], (error) => {
+        if (error) console.log(error);
+    });
+
+    db.run('insert into TB_PESSOAS (name) values (?)', ['José Nascimento Silva'], (error) => {
+        if (error) console.log(error);
+    });
+
+    db.run('insert into TB_PESSOAS (name) values (?)', ['Maria Antonieta Braga'], (error) => {
+        if (error) console.log(error);
+    });
+    
+    console.log("TB_PESSOAS")
+
+    db.all('select * from TB_PESSOAS', (error, rows) => {
+        console.log(error, rows);
+    });
+});
